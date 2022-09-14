ff88fc0c27b137f862330811150af3df8bfd0290
justin.fiedler <justin.fiedler@amplitude.com>
2022-09-14 18:24:26 +0000

    AMP-59152 add scripts to run examples, add examples to CI workflow

--- react-app/src/App.test.tsx
+++ react-app/src/App.test.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+beforeEach(() => {
+  // jest.spyOn(console, 'log').mockImplementation(() => {});
+});
+
 test('renders learn react link', () => {
+  console.log = jest.fn()
   render(<App />);
   const linkElement = screen.getByText(/Amplitude SDK Unification/i);
   expect(linkElement).toBeInTheDocument();

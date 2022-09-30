3e5d7951ca7d885d0fc2a82fc73917ed96b3badf
justin.fiedler <justin.fiedler@amplitude.com>
2022-09-30 02:03:23 +0000

    AMP-61281 fixed bad import

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 /**
  * Import strongly typed SDKs for convenience
  */
-import { amplitude, user, analytics, experiment, UserLoggedIn, Logger, NoLogger } from './amplitude/index';
+import { amplitude, user, analytics, experiment, UserLoggedIn, Logger, NoLogger } from './amplitude/browser';
 
 const { REACT_APP_LOGGING_DISABLED } = process.env;
 const useLogger = REACT_APP_LOGGING_DISABLED !== 'true';
